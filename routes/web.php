2e45f611d9c197a943fdffcc67702617d288fc05
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-06-09 16:31:33 +0100

    chore: add page for login only UI

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/agent/login', function () {
+    return view('agent.login');
+});
